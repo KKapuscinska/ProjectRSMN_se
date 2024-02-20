458b03ab24315b5c36baab55ec82aaf87c49a0fe
KKapuscinska <k.m.kapuscinska@gmail.com>
2024-02-20 19:08:51 +0100

    Added new successful login anf logout tests

--- src/Test/LoginTest.java
+++ src/Test/LoginTest.java
@@ -1,135 +1,138 @@
 package test;
 
+
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-
-import jdk.jfr.Description;
-
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-
-import static org.openqa.selenium.support.locators.RelativeLocator.*;
+import jdk.jfr.Description;
 
 public class LoginTest {
 
 WebDriver driver;
 	
-	@BeforeTest
+	@BeforeTest(groups = {"smoketests"})
     public void setup() {
         WebDriverManager.chromedriver().setup();
+        
         driver = new ChromeDriver();
         
 		driver.manage().window().maximize();
 		
-        driver.get("https://www.rossmann.pl/logowanie");
+		driver.get("https://www.rossmann.pl/");
     }
 	
-	@Test
-	@Description("User cannot log in without filling in login fields.")
-	public void emptyFieldsLoginFailure() throws InterruptedException {
-
-		driver.findElement(By.xpath("//span[text()='Zaloguj się']")).click();
-
-		Thread.sleep(1500);
-
-		Assert.assertEquals(driver
-				.findElement(with(By.cssSelector(".invalid-feedback")).below(driver.findElement(By.id("login-user"))))
-				.getText(), "Proszę wpisać poprawny adres e-mail lub nazwę użytkownika.");
-
-		Assert.assertEquals(driver
-				.findElement(with(By.cssSelector(".invalid-feedback")).below(driver.findElement(By.id("login-password"))))
-				.getText(), "Błąd w obecnym haśle.");
-	}
-
-	@Test
-	@Description("User cannot log in with a too short login.")
-	public void tooShortLoginFailure() throws InterruptedException {
-
-		driver.findElement(By.id("login-user")).sendKeys("r");
-
-		driver.findElement(By.id("login-password")).sendKeys("randomText");
-
-		driver.findElement(By.xpath("//span[text()='Zaloguj się']")).click();
-
-		Thread.sleep(1500);
-
-		Assert.assertEquals(driver
-				.findElement(with(By.cssSelector(".invalid-feedback")).below(driver.findElement(By.id("login-user"))))
-				.getText(), "Nazwa użytkownika powinna składać się z co najmniej 4 znaków.");
-	}
-
-	@Test
-	@Description("User cannot log in with a too long login.")
-	public void tooLongLoginFailure() throws InterruptedException {
-
-		driver.findElement(By.id("login-user")).sendKeys(
-				"Loremipsumdolorsitamet.consectetueradipiscingelit.Aeneancommodoligulaegetdolor.Aeneanmassa.Cumsociisn");
+	@Test(priority = 0, groups = {"smoketests"})
+	@Description("User can successfully log in via a popup login window.")
+	public void successfulLoginViaPopup() throws InterruptedException {
 		
-		driver.findElement(By.id("login-password")).sendKeys("randomText");
-		Assert.assertEquals(driver.findElement(By.id("login-password")).getAttribute("value"), "randomText");
-
-		driver.findElement(By.xpath("//span[text()='Zaloguj się']")).click();
-
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		
+		
+		Actions actions = new Actions(driver);
+		actions.moveToElement(driver.findElement(By.cssSelector("a[title='Konto użytkownika']")))
+			.build()
+			.perform();	
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[title='Konto użytkownika']+.nav-user__dropdown")));
 		Thread.sleep(1500);
-
-		Assert.assertEquals(driver
-				.findElement(with(By.cssSelector(".invalid-feedback")).below(driver.findElement(By.id("login-user"))))
-				.getText(), "Nazwa użytkownika powinna składać się z maksymalnie 100 znaków.");
-	}
-
-	@Test
-	@Description("User cannot log in with an incorrect password.")
-	public void incorrectPasswordLoginFailure() throws InterruptedException {
-
-		driver.findElement(By.id("login-user")).sendKeys("cytest1");
-		Assert.assertEquals(driver.findElement(By.id("login-user")).getAttribute("value"), "cytest1");
-
-		driver.findElement(By.id("login-password")).sendKeys("randomText");
-		Assert.assertEquals(driver.findElement(By.id("login-password")).getAttribute("value"), "randomText");
-
+		driver.findElement(By.xpath("//button[text()='Zaloguj się']")).click();
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("login-form")));
+	    
+		driver.findElement(By.id("login-user")).sendKeys("automation224@gmail.com");
+		driver.findElement(By.id("login-password")).sendKeys("Tester123");
+		
 		driver.findElement(By.xpath("//span[text()='Zaloguj się']")).click();
-
-		Thread.sleep(1500);
-
-		Assert.assertEquals(driver
-				.findElement(with(By.cssSelector(".invalid-feedback")).below(driver.findElement(By.id("login-user"))))
-				.getText(), "Niepoprawne dane logowania.");
-
+		
+		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.className("login-form")));
+		
+		driver.findElement(By.cssSelector("a[title='Konto użytkownika']")).click();
+		
+		wait.until(ExpectedConditions.urlContains("/profil/ustawienia-konta"));
+		
 	}
+	
+	@Test(priority = 2)
+	@Description("User can successfully log in via the login page.")
+	public void successfulLoginViaPage() throws InterruptedException {
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		
+		driver.findElement(By.cssSelector("a[title='Konto użytkownika']")).click();
+		
+		wait.until(ExpectedConditions.urlContains("/logowanie"));
+		
+		driver.findElement(By.id("login-user")).sendKeys("automation224@gmail.com");
 
+		driver.findElement(By.id("login-password")).sendKeys("Tester123");
+		
+		driver.findElement(By.xpath("//span[text()='Zaloguj się']")).click();
+		
+		wait.until(ExpectedConditions.urlContains("/profil/ustawienia-konta"));
+	}
+	
+	
+	@Test(priority = 1, dependsOnMethods={"successfulLoginPopup()"}, groups = {"smoketests"})
+	@Description("User can successfully log out using the logout button in Profile.")
+	public void successfulLogout() throws InterruptedException {
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		
+		Actions actions = new Actions(driver);
+		actions.moveToElement(driver.findElement(By.cssSelector("a[title='Konto użytkownika']")))
+			.build()
+			.perform();			
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[title='Konto użytkownika']+.nav-user__dropdown")));
+		
+		driver.findElement(By.xpath("//button[text()='Wyloguj']")).click();
+		
+		Thread.sleep(1500);
+		
+		Assert.assertEquals(driver.getCurrentUrl(), "https://www.rossmann.pl/");
+		
+		driver.findElement(By.cssSelector("a[title='Konto użytkownika']")).click();
+		
+		Thread.sleep(1500);
+		
+		Assert.assertEquals(driver.getTitle(), "Zaloguj się | Drogeria Rossmann.pl");
+		
+	}
+	
 	@Test
 	@Description("Verify password visibility functionality.")
 	public void verifyPasswordVisibilityFunctionality() throws InterruptedException {
 
-		driver.findElement(By.id("login-user")).sendKeys("randomText");
-		Assert.assertEquals(driver.findElement(By.id("login-user")).getAttribute("value"), "randomText");
-
+		driver.get("https://www.rossmann.pl/logowanie");
+		
 		driver.findElement(By.id("login-password")).sendKeys("randomText");
-		Assert.assertEquals(driver.findElement(By.id("login-password")).getAttribute("value"), "randomText");
-
-		driver.findElement(By.xpath("//span[text()='Zaloguj się']")).click();
-
+		
+		Assert.assertEquals(driver.findElement(By.id("login-password"))
+				.getAttribute("type"), "password");
+		
+		driver.findElement(By.cssSelector(".input-group-text")).click();
+		
 		Thread.sleep(1500);
 
-		Assert.assertEquals(driver
-				.findElement(with(By.cssSelector(".invalid-feedback")).below(driver.findElement(By.id("login-user"))))
-				.getText(), "Niepoprawne dane logowania.");
+		Assert.assertEquals(driver.findElement(By.id("login-password"))
+				.getAttribute("type"), "text");
+		
 
-	}
-
-	@AfterMethod
-	public void afterMethod() {
-	        driver.findElement(By.id("login-user")).clear();
-	        driver.findElement(By.id("login-password")).clear();
 	}
 	
-	@AfterTest
+	@AfterTest(groups = {"smoketests"})
 	public void tearDown() {
 		
 	    if (driver != null) {
@@ -137,5 +140,4 @@ WebDriver driver;
 	        driver.quit();
 	    }
 	}	
-
 }
