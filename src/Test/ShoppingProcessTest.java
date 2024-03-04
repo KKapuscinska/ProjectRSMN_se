00cd02ec9716b7abcc4623fd2da5408c6b4889e6
KKapuscinska <k.m.kapuscinska@gmail.com>
2024-03-04 17:36:56 +0100

    Added addMaxQuantityToShoppingCart test

--- src/Test/ShoppingProcessTest.java
+++ src/Test/ShoppingProcessTest.java
@@ -6,6 +6,7 @@ import java.time.Duration;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -16,6 +17,7 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import java.util.Random;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -29,6 +31,7 @@ public class ShoppingProcessTest {
 		driver = new ChromeDriver();
 
 		driver.manage().window().maximize();
+		
 	}
 
 	@BeforeMethod(groups = {"smoketests"})
@@ -44,12 +47,14 @@ public class ShoppingProcessTest {
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 
 		String productName = null;
+		
+		// Adding to the cart first available product 
 		List<WebElement> productList = driver.findElements(By.cssSelector(".product-list__col--thirds"));
-
 		for (WebElement product : productList) {
 			List<WebElement> cartIconList = product.findElements(By.cssSelector(".nav-user__icon"));
 
 			if (cartIconList.size() > 0) {
+				// Grabbing name of first available product 
 				WebElement productNameElement = product.findElement(By.cssSelector(".tile-product__name strong"));
 				productName = productNameElement.getText();
 				WebElement cartIconElement = product.findElement(By.cssSelector(".nav-user__icon"));
@@ -68,8 +73,10 @@ public class ShoppingProcessTest {
 
 		String cartProductName = cartProductNameElement.getText();
 
+		// Verifying if the product added to the cart matches the product in the cart
 		Assert.assertEquals(productName, cartProductName);
 
+		// Click to remove btn
 		driver.findElement(By.cssSelector(".btn-del")).click();
 		Assert.assertEquals(driver.findElement(By.cssSelector(".h3")).getText(), "Twój koszyk jest pusty");
 	}
@@ -80,8 +87,8 @@ public class ShoppingProcessTest {
 
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 
+		// Adding to the cart first available product 
 		List<WebElement> products = driver.findElements(By.cssSelector(".product-list__col--thirds .nav-user__icon"));
-
 		if (products.size() > 0) {
 			WebElement firstProduct = products.get(0);
 			firstProduct.click();
@@ -120,8 +127,8 @@ public class ShoppingProcessTest {
 
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 
+		// Adding to the cart first available product 
 		List<WebElement> products = driver.findElements(By.cssSelector(".product-list__col--thirds .nav-user__icon"));
-
 		if (products.size() > 0) {
 			WebElement firstProduct = products.get(0);
 			firstProduct.click();
@@ -145,13 +152,13 @@ public class ShoppingProcessTest {
 
 	@Test
 	@Description("Verify that the user can increase the quantity of a product in the shopping cart.")
-	public void increaseProductQuantityInShoppingCart() {
+	public void increaseProductQuantityInShoppingCart() throws InterruptedException {
 		driver.get("https://www.rossmann.pl/szukaj");
 
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 
+		// Adding to the cart first available product 
 		List<WebElement> products = driver.findElements(By.cssSelector(".product-list__col--thirds .nav-user__icon"));
-
 		if (products.size() > 0) {
 			WebElement firstProduct = products.get(0);
 			firstProduct.click();
@@ -165,17 +172,27 @@ public class ShoppingProcessTest {
 
 		driver.findElement(By.cssSelector(".cart-product__quantity")).click();
 
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".sri-select__items-container")));
+		// Selecting random available number of articles from a dropdown list
+		List<WebElement> dropdownOptions = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("div[role='rowgroup'] div")));
 
-		WebElement element = driver.findElement(By.cssSelector("div[role='rowgroup'] div:nth-child(3)"));
-		element.click();
+		Random rand = new Random();
+		int randomIndex = rand.nextInt(dropdownOptions.size() - 2) + 2;
 
-		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(".sri-select__items-container")));
+		WebElement randomElement = dropdownOptions.get(randomIndex);
 
-		Assert.assertEquals(driver.findElement(By.cssSelector(".sri-select__selected")).getText(), "2");
+		String randomElementText = randomElement.getText();
 
-		Assert.assertEquals(driver.findElement(By.cssSelector(".nav-user__quantity")).getText(), "2");
+		randomElement.click();
+		
+		Thread.sleep(1500);
 
+		// Checking if the number of products has been selected correctly
+		Assert.assertEquals(driver.findElement(By.cssSelector(".sri-select__selected")).getText(), randomElementText);
+
+		// Checking if the number of products has been change above cart icon
+		Assert.assertEquals(driver.findElement(By.cssSelector(".nav-user__quantity")).getText(), randomElementText);
+
+		// Clean the shopping cart
 		driver.findElement(By.cssSelector(".btn-del")).click();
 		Assert.assertEquals(driver.findElement(By.cssSelector(".h3")).getText(), "Twój koszyk jest pusty");
 	}
@@ -186,8 +203,8 @@ public class ShoppingProcessTest {
 
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 
+		// Adding to the cart first available product 
 		List<WebElement> products = driver.findElements(By.cssSelector(".product-list__col--thirds .nav-user__icon"));
-
 		if (products.size() > 0) {
 			WebElement firstProduct = products.get(0);
 			firstProduct.click();
@@ -205,36 +222,82 @@ public class ShoppingProcessTest {
 
 		driver.findElement(By.cssSelector(".cart-product__quantity")).click();
 
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".sri-select__items-container")));
+		// Selecting the highest available number of articles from a dropdown list
+		List<WebElement> dropdownOptions = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("div[role='rowgroup'] div")));
 
-		WebElement element = driver.findElement(By.cssSelector("div[role='rowgroup'] div:nth-child(3)"));
-		element.click();
+		WebElement lastVisibleElement = dropdownOptions.get(dropdownOptions.size() - 1);
+		
+		String lastVisibleElementText = lastVisibleElement.getText();
+		
+		int lastVisibleElementValue = Integer.parseInt(lastVisibleElementText);
+
+		lastVisibleElement.click();
 
 		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(".sri-select__items-container")));
 
-		Assert.assertEquals(driver.findElement(By.cssSelector(".sri-select__selected")).getText(), "2");
+		Assert.assertEquals(driver.findElement(By.cssSelector(".sri-select__selected")).getText(), lastVisibleElementText);
 
+		// Checking if the cart total matches the products price
+		
 		String productPriceAfter = driver.findElement(By.cssSelector(".cart-product__price")).getText();
 		String cartValueAfter = driver.findElement(By.cssSelector(".price-details__value-total")).getText();
 
 		double priceBefore = Double.parseDouble(productPriceBefore.replaceAll("[^0-9.,]+", "").replace(",", "."));
 		double priceAfter = Double.parseDouble(productPriceAfter.replaceAll("[^0-9.,]+", "").replace(",", "."));
 
-		Assert.assertEquals(priceAfter, priceBefore * 2, 0.01);
+		Assert.assertEquals(priceAfter, priceBefore * lastVisibleElementValue, 0.01);
 
 		double cartBefore = Double.parseDouble(cartValueBefore.replaceAll("[^0-9.,]+", "").replace(",", "."));
 		double cartAfter = Double.parseDouble(cartValueAfter.replaceAll("[^0-9.,]+", "").replace(",", "."));
 
-		Assert.assertEquals(cartAfter, cartBefore * 2, 0.01);
+		Assert.assertEquals(cartAfter, cartBefore * lastVisibleElementValue, 0.01);
 
+		// Clean the shopping cart
 		driver.findElement(By.cssSelector(".btn-del")).click();
 		Assert.assertEquals(driver.findElement(By.cssSelector(".h3")).getText(), "Twój koszyk jest pusty");
 
 	}
 
-	@Description("Add maximum quantiti of one product to  shopping cart.")
-	public void addMaxQuantityToShoppingCart() {
+	@Test
+	@Description("Add maximum quantity of one product to shopping cart and verify popup message.")
+	public void addMaxQuantityToShoppingCart() throws InterruptedException {
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		
+		boolean maxProductsReached = false;
+
+		while (!maxProductsReached) {
 			
+			// Adding to the cart first available product 
+		    List<WebElement> products = driver.findElements(By.cssSelector(".product-list__col--thirds .nav-user__icon"));
+		    if (products.size() > 0) {
+		        WebElement firstProduct = products.get(0);
+		        firstProduct.click();
+		        
+		        try {
+		        	
+		        	// Check if popup with maximum quantity message appears
+	                WebElement maxProductsPopup = driver.findElement(By.cssSelector("div.Toastify__toast-body"));
+	                String popupText = maxProductsPopup.getText();
+	                if (popupText.equals("Przepraszamy, nie mamy więcej sztuk tego produktu")) {
+	                    System.out.println("Test passed: Popup message matches expected text.");
+	                    maxProductsReached = true; // Set flag to true when popup is visible
+	                } else {
+	                    System.out.println("Test failed: Popup message does not match expected text.");
+	                    break;
+	                }
+	            } catch (NoSuchElementException e) {
+	            	Thread.sleep(300);
+	            	// Continue adding product if popup is not visible
+	            }
+	        }
+		}
+		
+		// Clear the cart
+		driver.get("https://www.rossmann.pl/zamowienie/koszyk");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".cart-product__name strong")));
+		driver.findElement(By.cssSelector(".btn-del")).click();
+		Assert.assertEquals(driver.findElement(By.cssSelector(".h3")).getText(), "Twój koszyk jest pusty");
 	}
 	
 	@Description("Verify that the user can proceed to checkout from the shopping cart.")
