a99619ca2d57660d39149e2b9c28685a4451de37
KKapuscinska <k.m.kapuscinska@gmail.com>
2024-05-12 17:47:19 +0200

    Implemented JSON data parsing with Jackson Databind.

--- src/test/java/basetest/BaseTest.java
+++ src/test/java/basetest/BaseTest.java
@@ -1,13 +1,22 @@
 package test.java.basetest;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Properties;
+
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import main.java.pages.HomePage;
@@ -31,12 +40,23 @@ public class BaseTest {
 		else if(browserName.equalsIgnoreCase("firefox"))
 		{
 			WebDriverManager.firefoxdriver().setup();
-			driver = new FirefoxDriver();
+			driver = new FirefoxDriver(); 
 		}
 		
 		driver.manage().window().maximize();
 		
 		return driver;
+	}
+	
+	public List<HashMap<String, String>> getJsonDataToMap(String filePath) throws IOException
+	{
+		String jsonContent = FileUtils.readFileToString(new File(filePath), 
+				StandardCharsets.UTF_8);
+		
+		ObjectMapper mapper = new ObjectMapper();
+		List<HashMap<String, String>> data = mapper.readValue(jsonContent, new TypeReference<List<HashMap<String, String>>>(){
+		});
+		return data;
 	}
 	
 	@BeforeTest
@@ -49,7 +69,7 @@ public class BaseTest {
 		return homePage;
 	}
 	
-	@AfterTest
+	//@AfterTest
 	public void quitDriver() {
         if (driver != null) {
             driver.quit();
@@ -61,3 +81,5 @@ public class BaseTest {
 	
 	
 }
+
+
